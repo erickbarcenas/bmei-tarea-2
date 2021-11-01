79e4a0e386ac2c7c1ffda6317a0fbfa01626a555
erickbarcenas <barcenas.dev@gmail.com>
2021-11-01 16:53:18 -0600

    BASE INIT

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibarcenas/rosdev/bmei-tarea-2/src
+# Build directory: /home/ibarcenas/rosdev/bmei-tarea-2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ibarcenas/rosdev/bmei-tarea-2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ibarcenas/rosdev/bmei-tarea-1/devel;/home/ibarcenas/rosdev/catkin_ws/devel;/home/ibarcenas/rosdev/simulation_ws/devel;/home/ibarcenas/rosdev/robot_ws/devel;/opt/ros/noetic'.split(';') if '/home/ibarcenas/rosdev/bmei-tarea-1/devel;/home/ibarcenas/rosdev/catkin_ws/devel;/home/ibarcenas/rosdev/simulation_ws/devel;/home/ibarcenas/rosdev/robot_ws/devel;/opt/ros/noetic' != '' else []
